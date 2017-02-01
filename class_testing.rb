63c65d7541cda065cf311747c92d79c2bbb598ee
wilson <wt@wtcom.local>
2017-02-01 11:34:52 +0800

    Add age check method

--- class_testing.rb
+++ class_testing.rb
@@ -13,6 +13,10 @@ class Person
     puts "#{@name} is walking."
   end
 
+  def is_old?
+    @age > 40
+  end
+
   def self.count_of
     puts "This only works when called on class."
     puts "#{@@num_of_persons} Person class has been created so far."
