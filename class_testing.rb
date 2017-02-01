aa125c5fd9df25bf0e12f212082ce7cfd5006e07
wilson <wt@wtcom.local>
2017-02-01 11:23:59 +0800

    Add some more stuff to Person class

--- class_testing.rb
+++ class_testing.rb
@@ -1,9 +1,20 @@
 class Person
   attr_accessor :name
   attr_reader :age
+  @@num_of_persons = 0
 
   def initialize(name, age)
     @name = name
     @age = age
+    @@num_of_persons += 1
+  end
+
+  def walk
+    puts "#{@name} is walking."
+  end
+
+  def self.test1
+    puts "This only works when called on class."
+    puts "#{@@num_of_persons} Person class has been created so far."
   end
 end
