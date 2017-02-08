4d5ef97ec4e848cf35f6feaf316354a1cbf32b0e
icebear <wt@wtcom.local>
2017-02-08 17:57:36 +0800

    Revert back to before trying db:seed

--- blog/app/models/blog.rb
+++ blog/app/models/blog.rb
@@ -1,12 +1,12 @@
 class Blog < ApplicationRecord
-  # validates :title, uniqueness: true,
-  #                   length: { minimum: 20 },
-  #                   presence: true
-  #
-  # validates :author, presence: true,
-  #                   length: { minimum: 5 },
-  #                   format: { without: /\d/, message: "No numbers allowed"}
-  #
-  # validates :post, presence: true,
-  #                   length: {minimum: 250, maximum: 2000}
+  validates :title, uniqueness: true,
+                    length: { minimum: 20 },
+                    presence: true
+
+  validates :author, presence: true,
+                    length: { minimum: 5 },
+                    format: { without: /\d/, message: "No numbers allowed"}
+
+  validates :post, presence: true,
+                    length: {minimum: 250, maximum: 2000}
 end
