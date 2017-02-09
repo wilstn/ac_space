186ab25823795985f738974d1281a8a4e1424750
icebear <wt@wtcom.local>
2017-02-09 13:02:23 +0800

    Modify slight markdown for table

--- blog/app/helpers/application_helper.rb
+++ blog/app/helpers/application_helper.rb
@@ -11,12 +11,14 @@ module ApplicationHelper
     extensions = {
       autolink:           true,
       superscript:        true,
-      disable_indented_code_blocks: true
+      disable_indented_code_blocks: true,
+      tables: true
     }
 
     renderer = Redcarpet::Render::HTML.new(options)
     markdown = Redcarpet::Markdown.new(renderer, extensions)
 
     markdown.render(text).html_safe
+
   end
 end
