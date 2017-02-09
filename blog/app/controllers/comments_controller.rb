c2e2b5b3393ecad07aaa68de3876669937d5576b
icebear <wt@wtcom.local>
2017-02-09 17:23:55 +0800

    Add basic ability to edit and delete comment

--- blog/app/controllers/comments_controller.rb
+++ blog/app/controllers/comments_controller.rb
@@ -8,20 +8,41 @@ class CommentsController < ApplicationController
   end
 
   def new
-    @comment = Comment.new(blog_id: params[:blog_id])
+    @blog = Blog.find(params[:blog_id])
+    @comment = @blog.comments.new
   end
 
   def edit
+    @blog = Blog.find(params[:blog_id])
+    @comment = Comment.find(params[:id])
+  end
+
+  def update
+    @blog = Blog.find(params[:blog_id])
+    @comment = Comment.find(params[:id])
+
+    if @comment.update(comment_params)
+      redirect_to blog_path(@blog)
+    else
+      render 'edit'
+    end
   end
 
   def create
-    @blog = Blog.find(params[:blog_id])
-    @comment = @blog.comments.create(comment_params)
-    redirect_to blog_path(@blog)
+    blog = Blog.find(params[:blog_id])
+    @comment = blog.comments.create(comment_params)
+    redirect_to blog_path(blog)
+  end
+
+  def destroy
+    blog = Blog.find(params[:blog_id])
+    @comment = blog.comments.find(params[:id])
+    @comment.destroy
+    redirect_to blog_path(blog)
   end
 
   private
     def comment_params
-      params.require(:comment).permit(:remark, :email, :user, :blog_id)
+      params.require(:comment).permit(:remark, :email, :user)
     end
 end
--- blog/config/routes.rb
+++ blog/config/routes.rb
@@ -3,5 +3,4 @@ Rails.application.routes.draw do
     resources :comments
   end
   root 'blogs#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
