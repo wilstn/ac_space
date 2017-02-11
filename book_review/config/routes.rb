eaa657359adfe5d1fd0b23f7087d1d0f648cce9e
icebear <wt@wtcom.local>
2017-02-11 15:08:08 +0800

    Add and configure routes for book_review

--- book_review/config/routes.rb
+++ book_review/config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'book#index'
+
+  resources :book do
+    resources :review
+    collection do
+      get 'search'
+      post 'search'
+    end
+  end
+  
 end
