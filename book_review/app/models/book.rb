0f6aadee129d4aceb8df80a6b1cc560da2de0ec1
icebear <wt@wtcom.local>
2017-02-11 16:41:58 +0800

    Add book and review models validations and associations

--- book_review/app/models/book.rb
+++ book_review/app/models/book.rb
@@ -1,3 +1,8 @@
 class Book < ApplicationRecord
-  validates :title, presence: true
+  has_many :reviews, dependent: :destroy
+
+  validates :title, presence: true, uniqueness: true
+  validates :author, presence: true
+  validates :genre, presence: true
+  validates :summary, presence: true
 end
