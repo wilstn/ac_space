afe20479773efb03afe6a65513bb4a5e8e7d3072
icebear <wt@wtcom.local>
2017-02-11 16:29:38 +0800

    Add basic create, edit and delete functions for book

---
+++ book_review/app/controllers/books_controller.rb
@@ -0,0 +1,52 @@
+class BooksController < ApplicationController
+
+  def index
+    @books = Book.all
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+
+    if @book.save
+      redirect_to books_path
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+
+    if @book.update(book_params)
+      redirect_to books_path
+    else
+      redirect_to edit_book_path(@book)
+    end
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+
+    redirect_to books_path
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :author, :summary, :genre)
+  end
+
+end
--- book_review/config/routes.rb
+++ book_review/config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'book#index'
+  root 'books#index'
 
-  resources :book do
-    resources :review
+  resources :books do
+    resources :reviews
     collection do
       get 'search'
       post 'search'
     end
   end
-  
+
 end
