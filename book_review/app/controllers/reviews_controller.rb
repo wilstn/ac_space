6e113eb97edd63db2c45cdb02b56d20c646177dd
icebear <wt@wtcom.local>
2017-02-13 01:05:05 +0800

    Add avg rating calculation for book

--- book_review/app/controllers/reviews_controller.rb
+++ book_review/app/controllers/reviews_controller.rb
@@ -13,6 +13,11 @@ class ReviewsController < ApplicationController
     @review = @book.review.new(review_params)
 
     if @review.save
+      avg = @book.avg_rating
+      counter = @book.review.count - 1
+      hello = (@review[:rating] + counter * avg)/(counter + 1)
+      @book.update(avg_rating: hello)
+
       redirect_to book_path(@book)
     else
       render 'new'
@@ -29,6 +34,13 @@ class ReviewsController < ApplicationController
     @review = Review.find(params[:id])
 
     if @review.update(review_params)
+      total_rates = 0.0
+      @book.review.each do |review_obj|
+        total_rates += review_obj.rating
+      end
+      hello = (total_rates / @book.review.count).round(1)
+      @book.update(avg_rating: hello)
+
       redirect_to book_path(@book)
     else
       render 'edit'
@@ -36,10 +48,19 @@ class ReviewsController < ApplicationController
   end
 
   def destroy
-    book = Book.find(params[:book_id])
-    @review = book.review.find(params[:id])
+    @book = Book.find(params[:book_id])
+    @review = @book.review.find(params[:id])
     @review.destroy
-    redirect_to book_path(book)
+
+    total_rates = 0.0
+
+    @book.review.each do |review_obj|
+      total_rates += review_obj.rating
+    end
+    hello = (total_rates / @book.review.count).round(1)
+    @book.update(avg_rating: hello)
+
+    redirect_to book_path(@book)
   end
 
   private
--- book_review/db/schema.rb
+++ book_review/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170212155321) do
+ActiveRecord::Schema.define(version: 20170212161025) do
 
   create_table "average_caches", force: :cascade do |t|
     t.integer  "rater_id"
